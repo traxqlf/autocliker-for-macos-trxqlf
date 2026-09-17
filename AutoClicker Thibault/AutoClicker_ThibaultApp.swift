3be8675e0473145fc84610d2c70ac660dd07f673
traxqlf <titispaceone@gmail.com>
2026-09-17 10:01:38 +0200

    Initial Commit

---
+++ AutoClicker Thibault/AutoClicker_ThibaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoClicker_ThibaultApp.swift
+//  AutoClicker Thibault
+//
+//  Created by Thibault on 17/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct AutoClicker_ThibaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoClicker Thibault/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoClicker Thibault
+//
+//  Created by Thibault on 17/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
